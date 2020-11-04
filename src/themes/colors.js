c0567be3890ae0daebe5ac6f591937998f78b17f
Peter <hoangvanlam9988@gmail.com>
2020-11-04 12:03:50 +0700

    create Color file global

---
+++ src/themes/colors.js
@@ -0,0 +1,122 @@
+/**
+ * @author [Peter]
+ * @email [hoangvanlam9988@mail.com]
+ * @create date 2020-11-04 11:30:30
+ * @modify date 2020-11-04 12:03:20
+ * @desc [description]
+ */
+
+export const color = {
+  primary: {
+    main: '#FF6D00',
+    dark: '#E64A19',
+    light: '#FF8A65',
+    constrast: '#ffffff',
+    background: '#FF6D00 (8% alpha)',
+  },
+  secondary: {
+    main: '#F2453D',
+    dark: '#D32F2F',
+    light: '#E57373',
+    constrast: '#ffffff',
+    background: '#F2453D (8% alpha)',
+  },
+  tertiary_01: {
+    main: '#4CAF50',
+    dark: '#388E3C',
+    light: '#81C784',
+    constrast: '#ffffff',
+    text: '#4CAF50 (60% darken)',
+    background: '#4CAF50 (90% lighten)',
+  },
+  tertiary_02: {
+    main: '#2196F3',
+    dark: '#0B79D0',
+    light: '#BBDEFB',
+    constrast: '#ffffff',
+    text: '#2196F3 (60% darken)',
+    background: '#2196F3 (90% lighten)',
+  },
+  point: {
+    main: '#FFEB3B',
+    dark: '#FBC02D',
+    light: '#FFF59D',
+    constrast: '#ffffff',
+    background: '#F2453D (8% alpha)',
+  },
+  success: {
+    main: '#4CAF50',
+    dark: '#388E3C',
+    light: '#81C784',
+    constrast: '#ffffff',
+    text: '#4CAF50 (60% darken)',
+    background: '#4CAF50 (90% lighten)',
+  },
+  info: {
+    main: '#2196F3',
+    dark: '#0B79D0',
+    light: '#BBDEFB',
+    constrast: '#ffffff',
+    text: '#2196F3 (60% darken)',
+    background: '#2196F3 (90% lighten)',
+  },
+  warning: {
+    main: '#FF9800',
+    dark: '#F57C00',
+    light: '#FFB74D',
+    constrast: '#ffffff',
+    text: '#FF9800 (60% darken)',
+    background: '#FF9800 (90% lighten)',
+  },
+  error: {
+    main: '#F44336',
+    dark: '#D32F2F',
+    light: '#E57373',
+    constrast: '#ffffff',
+    text: '#F44336 (60% darken)',
+    background: '#F44336 (90% lighten)',
+  },
+  grey: {
+    _50: '#FAFAFA',
+    _100: '#F5F5F5',
+    _200: '#EEEEEE',
+    _300: '#E0E0E0',
+    _400: '#BDBDBD',
+    _500: '#9E9E9E',
+    _600: '#757575',
+    _700: '#616161',
+    _800: '#424242',
+    _800P: '#616161',
+    A100: '#D5D5D5',
+    A200: '#AAAAAA',
+    A400: '#616161',
+    A700: '#303030',
+  },
+  text: {
+    primary: 'rgba(0, 0, 0, 0.87)',
+    secondary: 'rgba(0, 0, 0, 0.54)',
+    disabled: 'rgba(0, 0, 0, 0.38)',
+    hint: 'rgba(0, 0, 0, 0.38)',
+  },
+  action: {
+    active: 'rgba(0, 0, 0, 0.54)',
+    hover: 'rgba(0, 0, 0, 0.04)',
+    selected: 'rgba(0, 0, 0, 0.08)',
+    disabled: 'rgba(0, 0, 0, 0.26)',
+    disabled_bg: 'rgba(0, 0, 0, 0.12)',
+    focused: 'rgba(0, 0, 0, 0.12)',
+  },
+  misc: {
+    black: '#000000',
+    white: '#FFFFFF',
+    paper: '#FFFFFF',
+    default: '#FAFAFA',
+    divider: 'rgba(0, 0, 0, 0.12)',
+    backdrop: 'rgba(0, 0, 0, 0.5)',
+    filled_input: 'rgba(0, 0, 0, 0.09)',
+    tooltip: 'rgba(97, 97, 97, 0.9)',
+    snackbar: '#323232',
+    rating_full: '#FFEB3B',
+    rating_full_radient: '#FFEB3B',
+  },
+};
