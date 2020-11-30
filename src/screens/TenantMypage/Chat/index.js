0fa969f5f37d47dab3b46efcb97f24fb45b40d34
Life <tutaihp95@gmail.com>
2020-11-30 19:26:19 +0700

    C chat

--- src/screens/TenantMypage/Chat/index.js
+++ src/screens/TenantMypage/Chat/index.js
@@ -22,6 +22,7 @@ import {
   Paragraph,
   Button,
   List,
+  TextInput,
 } from 'react-native-paper';
 
 // Local Imports
@@ -81,17 +82,95 @@ class Chatting extends Component {
           />
         </Appbars>
         <ScrollView>
-          <View style>
+          <View style={SS.header}>
             <List.Section>
               <List.Accordion
                 title="에이씨티앤코아물류"
                 titleStyle={SS.name}
                 style={SS.headerChat}
                 left={() => <Image source={card} style={SS.avatar} />}>
-                <List.Item title="First item" />
-                <List.Item title="Second item" />
+                <List.Item
+                  titleStyle={{ display: 'none' }}
+                  style={SS.popupBtn}
+                  descriptionStyle={SS.btnConfirm}
+                  description={() => (
+                    <View style={[DefaultStyle._listBtn, SS.listBtn]}>
+                      <TouchableOpacity
+                        style={[
+                          DefaultStyle._btnOutline,
+                          DefaultStyle._btnLeft,
+                          { borderColor: '#000000' },
+                        ]}
+                        onPress={() => console.log('계약 요청 취소')}>
+                        <Text
+                          style={[
+                            DefaultStyle._textButton,
+                            { color: '#000000' },
+                          ]}>
+                          계약 요청 취소
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        style={[
+                          DefaultStyle._btnInline,
+                          DefaultStyle._btnRight,
+                        ]}
+                        onPress={() => {}}>
+                        <Text
+                          style={[
+                            DefaultStyle._textButton,
+                            { color: '#ffffff' },
+                          ]}>
+                          계약서 작성
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  )}
+                />
               </List.Accordion>
             </List.Section>
+          </View>
+          <View style={SS.chatting}>
+            <View style={SS.dateTop}>
+              <Text style={SS.textDateTop}>2020년 10월 30일</Text>
+            </View>
+            <View style={SS.user}>
+              <View style={SS.info}>
+                <View>
+                  <Image source={card} style={SS.avatar} />
+                  <View style={SS.status} />
+                </View>
+                <Text style={SS.name}>에이씨티앤코아물류</Text>
+                <Text style={SS.time}>4분 전</Text>
+              </View>
+              <View style={SS.body}>
+                <Text style={SS.content}>안녕하세요!</Text>
+                <Text style={SS.content}>
+                  에이씨티앤코아물류입니다. 계약 요청을 해주셔서 감사합니다.
+                  계약 진행 전, 창고 및 계약 관련 문의사항을 채팅으로 문의해
+                  주세요.
+                </Text>
+              </View>
+            </View>
+            <View style={[SS.user, SS.userMe]}>
+              <View style={SS.info}>
+                <Text style={SS.time}>방금 전</Text>
+              </View>
+              <View style={[SS.body, SS.bodyMe]}>
+                <Text style={[SS.content, SS.contentMe]}>
+                  안녕하세요, 계약 진행 관련하여 몇 가지 여쭤볼 게 있어서
+                  채팅으로 남깁니다.
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={SS.footer}>
+            <View style={SS.footerItem}>
+              <View style={SS.inputChat}>
+                <TextInput />
+              </View>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
--- src/screens/TenantMypage/Chat/style.js
+++ src/screens/TenantMypage/Chat/style.js
@@ -11,10 +11,18 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export const styles = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    top: 0,
+    zIndex: 99,
+    flex: 1,
+    width: windowWidth,
+    backgroundColor: '#ffffff',
+  },
   headerChat: {
     padding: 16,
     margin: 0,
-    marginTop: 0,
+    marginTop: -10,
   },
   avatar: {
     width: 40,
@@ -27,5 +35,101 @@ export const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: '500',
     color: color.text.primary,
+    paddingLeft: 6,
+  },
+  popupBtn: {
+    padding: 0,
+    paddingLeft: 0,
+  },
+
+  btnConfirm: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginLeft: 0,
+  },
+  listBtn: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginLeft: 0,
+    marginTop: 0,
+    marginBottom: 16,
+  },
+  chatting: {
+    padding: 16,
+    backgroundColor: 'rgba(0, 0, 0, 0.06)',
+  },
+  dateTop: {
+    marginTop: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textDateTop: {
+    borderColor: 'rgba(33, 33, 33, 0.12)',
+    borderWidth: 1,
+    borderRadius: 15,
+    color: 'rgba(0, 0, 0, 0.54)',
+    padding: 10,
+    paddingTop: 4,
+    paddingBottom: 3,
+    lineHeight: 23,
+    fontFamily: 'NotoSansCJKkr-Medium',
+    fontSize: 13,
+    fontWeight: '500',
+  },
+  user: {
+    flex: 1,
+    marginBottom: 16,
+  },
+  userMe: {
+    alignItems: 'flex-end',
+  },
+  info: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  time: {
+    lineHeight: 19,
+    fontFamily: 'NotoSansCJKkr-Medium',
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'rgba(0, 0, 0, 0.54)',
+    paddingLeft: 6,
+  },
+  status: {
+    width: 12,
+    height: 12,
+    borderRadius: 12,
+    backgroundColor: '#4caf50',
+    borderWidth: 2,
+    borderColor: '#fff',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  body: {
+    maxWidth: '70%',
+    padding: 16,
+    backgroundColor: '#fafafa',
+    borderRadius: 21,
+    borderTopLeftRadius: 0,
+    marginTop: 12,
+  },
+  bodyMe: {
+    backgroundColor: '#ff6d00',
+    borderTopLeftRadius: 21,
+    borderTopRightRadius: 0,
+  },
+
+  content: {
+    fontFamily: 'NotoSansCJKkr-Medium',
+    fontSize: 14,
+    fontWeight: '500',
+    color: color.text.primary,
+    paddingLeft: 6,
+    lineHeight: 20,
+
+  },
+  contentMe: {
+    color: '#ffffff',
   },
 });
