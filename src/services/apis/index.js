48c1ab1323eb35c7f17a97c28ba626f362563ec7
Jon <phamngochau1506@gmail.com>
2020-12-29 17:23:57 +0700

    update code call page FAQ

--- src/services/apis/index.js
+++ src/services/apis/index.js
@@ -3,5 +3,6 @@ import * as Account from './Account';
 import * as Warehouse from './Warehouse';
 import * as WarehouseOut from './WarehouseOut';
 import * as Annoucement from './Annoucement';
+import * as FAQ from './FAQ';
 
-export { Sample, Account, Warehouse, WarehouseOut, Annoucement };
+export { Sample, Account, Warehouse, WarehouseOut, Annoucement, FAQ };
