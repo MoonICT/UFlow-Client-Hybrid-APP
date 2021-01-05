a99858fd282f057c4dac876df8207233999f6f8a
Peter <hoangvanlam9988@gmail.com>
2021-01-05 09:41:34 +0700

    no message

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -81,7 +81,6 @@ import RequestContract from '@Screeens/Mypage/RequestContract';
 import More from '@Screeens/More';
 import Consulting from '@Screeens/Consulting';
 import Question from '@Screeens/Question';
-import RequestContract from '@Screeens/Mypage/RequestContract';
 import Inquiry from '@Screeens/Inquiry';
 import DetailInquiry from '@Screeens/Inquiry/DetailInquiry';
 
