f525f181cfa368f5c91235d46d0292c9b998fb98
Life <tutaihp95@gmail.com>
2021-02-04 16:12:23 +0700

    Fixbug m4-7

--- src/screens/DetailsWH/index.js
+++ src/screens/DetailsWH/index.js
@@ -1056,9 +1056,14 @@ class DetailWH extends Component {
             <View style={S.info}>
               <Text style={S.title}>위치</Text>
               <Text style={S.describeTitle}>
-                {whrgData.roadAddr
-                  ? `${whrgData.roadAddr.address} ${whrgData.roadAddr.detail}`
-                  : '-'}
+              {whrgData.roadAddr
+                ? `${whrgData.roadAddr.address} ${
+                    whrgData.roadAddr.detail === null ||
+                    whrgData.roadAddr.detail === undefined
+                      ? ''
+                      : whrgData.roadAddr.detail
+                  }`
+                : '-'}
               </Text>
               <View style={DefaultStyle._card}>
                 <View style={S.bodyCard}>
