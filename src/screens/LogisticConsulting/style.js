5d6d3610e3713756d18c044e90346a2c7934f68c
Life <tutaihp95@gmail.com>
2021-02-05 10:27:35 +0700

    Fixbug m2-7,m2-8

---
+++ src/screens/LogisticConsulting/style.js
@@ -0,0 +1,41 @@
+/**
+ * @create
+ * @modify
+ * @desc [description]
+ */
+
+import { StyleSheet, Dimensions } from 'react-native';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export const styles = StyleSheet.create({
+  container: {
+    width: windowWidth,
+    height: windowHeight - 30,
+    backgroundColor: 'white',
+  },
+  bodyView: {
+    padding: 16,
+  },
+  textContent: {
+    // fontFamily: 'NotoSansCJKkr-Regular',
+    fontSize: 14,
+    fontStyle: 'normal',
+    color: 'rgba(0, 0, 0, 0.87)',
+    lineHeight: 20,
+    marginTop: 24,
+  },
+  contentQuestion:{
+    fontSize: 12,
+  },
+  titleQuestion:{
+    margin: 0,
+    textAlign: 'left',
+    fontSize:20,
+    fontWeight:'500',
+  },
+  headerButon:{
+    // color: 'rgba(0, 0, 0, 0.47)',
+    color: '#ff6d00',
+  },
+});
