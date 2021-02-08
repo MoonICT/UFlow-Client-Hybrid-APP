0e199691d87e673efd9e1eab62198bd748708fdf
Life <tutaihp95@gmail.com>
2021-02-08 18:08:56 +0700

    Update upi sever

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,13 +5,13 @@ export const MY_PAGE_TAB_STATUS_KEY = 'myPageTabStatus';
 
 /** Production */
 // export const API_CLIENT_ADDRESS = 'https://www.uflow.kr';
-export const API_SERVER_ADDRESS = 'https://api.uflow.kr';
+// export const API_SERVER_ADDRESS = 'https://api.uflow.kr';
 // export const FILE_SERVER_ADDRESS = 'https://file.uflow.kr';
 
 /** Development */
 // export const API_CLIENT_ADDRESS = 'http://localhost:13000';
 export const API_CLIENT_ADDRESS = 'http://uflow.logisall.com';
 // export const API_SERVER_ADDRESS = 'http://192.168.1.101:3002';
-// export const API_SERVER_ADDRESS = 'http://apidev.uflow.logisall.com';
+export const API_SERVER_ADDRESS = 'http://apidev.uflow.logisall.com';
 export const FILE_SERVER_ADDRESS = 'http://file.lains.logisall.com:8500/uflowdev';
 
